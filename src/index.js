9d7be23e3b067bfbc77b5f6f2c0bfbcb4cf7ad3b
Tetsuaki Hamano / 浜野 哲明 <tetsuaki.hamano@gmail.com>
2022-10-15 09:12:38 +0000

    Revert "Refactor build & enqueue flow"
    This reverts commit 6c4b59eacc0973248ed00ba44bcb6060c691cbb6.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { registerBlockType } from '@wordpress/blocks';
 /**
  * Internal dependencies
  */
-import './editor.scss';
 import metadata from './block.json';
 import edit from './edit';
 import save from './save';
