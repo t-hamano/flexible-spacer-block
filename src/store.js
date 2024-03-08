df3b7f0a26e6d2b074e239627cdab4f17838bdea
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2024-03-08 18:29:23 +0900

    Quality: Replace deprecated `registerStore`

--- src/store.js
+++ src/store.js
@@ -1,40 +1,35 @@
 /**
  * WordPress dependencies
  */
-import { registerStore } from '@wordpress/data';
+import { createReduxStore, register } from '@wordpress/data';
 
 const DEFAULT_STATE = {
 	isResponsive: false,
 };
 
-const reducer = ( state = DEFAULT_STATE, action ) => {
-	if ( action.type === 'UPDATE_IS_RESPONSIVE' ) {
-		return {
-			...state,
-			isResponsive: ! state.isResponsive,
-		};
-	}
-
-	return state;
-};
-
-const actions = {
-	setIsResponsive( value ) {
-		return {
-			type: 'UPDATE_IS_RESPONSIVE',
-			value,
-		};
+const store = createReduxStore( 'flexible-spacer-block', {
+	reducer: ( state = DEFAULT_STATE, action ) => {
+		if ( action.type === 'UPDATE_IS_RESPONSIVE' ) {
+			return {
+				...state,
+				isResponsive: ! state.isResponsive,
+			};
+		}
+		return state;
 	},
-};
-
-const selectors = {
-	getIsResponsive( state ) {
-		return state.isResponsive;
+	selectors: {
+		getIsResponsive( state ) {
+			return state.isResponsive;
+		},
 	},
-};
-
-registerStore( 'flexible-spacer-block', {
-	reducer,
-	actions,
-	selectors,
+	actions: {
+		setIsResponsive( value ) {
+			return {
+				type: 'UPDATE_IS_RESPONSIVE',
+				value,
+			};
+		},
+	},
 } );
+
+register( store );
