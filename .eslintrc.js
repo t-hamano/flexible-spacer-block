a7b9f0dd73276389239957013c5f5f5931267420
Aki Hamano <54422211+t-hamano@users.noreply.github.com>
2026-02-28 20:37:02 +0900

    Update npm packages (#78)
    * Update npm packages
    
    * Fix eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,11 @@ module.exports = {
 		'import/no-extraneous-dependencies': 'off',
 		'react/jsx-boolean-value': 'error',
 		'react/jsx-curly-brace-presence': [ 'error', { props: 'never', children: 'never' } ],
+		'import/no-unresolved': 'off',
 		'@wordpress/dependency-group': 'error',
 		'@wordpress/no-unsafe-wp-apis': 'off',
+		'@wordpress/no-setting-ds-tokens': 'off',
+		'@wordpress/no-unknown-ds-tokens': 'off',
 		'@wordpress/i18n-text-domain': [
 			'error',
 			{
